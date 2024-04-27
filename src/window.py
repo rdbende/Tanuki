7a11394b7f25067315b6b95c6c191bb896460a45
rdbende <rdbende@proton.me>
2024-04-27 22:20:53 +0200

    Move ToolbarView inside the NavigationPage

--- src/window.py
+++ src/window.py
@@ -18,12 +18,19 @@ class MainWindow(Adw.ApplicationWindow):
     home_stack: Adw.ViewStack = Gtk.Template.Child()
     navigation_view: Adw.NavigationView = Gtk.Template.Child()
 
+    loading_stack: Gtk.Stack = Gtk.Template.Child()
+    spinner_box: Gtk.Box = Gtk.Template.Child()
+    logging_in_spinner: Gtk.Spinner = Gtk.Template.Child()
+
     primary_menu: Gio.MenuModel = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.setup_components()
+
+        session.connect("login-started", self.show_loading_spinner)
+        session.connect("login-completed", self.hide_loading_spinner)
 
         current_session = settings.props.current_session
         if not current_session:
@@ -36,20 +43,27 @@ class MainWindow(Adw.ApplicationWindow):
             lambda o, s: self.set_up_account() if not o.get_property(s) else None,
         )
 
+    def show_loading_spinner(self, *_) -> None:
+        self.loading_stack.set_visible_child(self.spinner_box)
+        self.logging_in_spinner.set_spinning(True)
+
+    def hide_loading_spinner(self, *_) -> None:
+        self.loading_stack.set_visible_child(self.home_stack)
+        self.logging_in_spinner.set_spinning(False)
+
     @Gtk.Template.Callback()
     def user_own_profile(self, *_):
         self.add_page(UserPage(session.get_account_info().username))
-
-    @Gtk.Template.Callback()
-    def back(self, *_):
-        self.navigation_view.pop()
 
     def set_up_account(self, *_) -> None:
         self.get_application().activate_action("add_new_account", GLib.Variant.new_boolean(True))
 
     def add_page(self, page: Adw.Bin) -> None:
         nav_page = Adw.NavigationPage(title="Page")
-        nav_page.set_child(page)
+        toolbar_view = Adw.ToolbarView()
+        toolbar_view.set_content(page)
+        toolbar_view.add_top_bar(Adw.HeaderBar())
+        nav_page.set_child(toolbar_view)
         self.navigation_view.push(nav_page)
 
     def setup_components(self):
